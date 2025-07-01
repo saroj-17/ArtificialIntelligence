6eff6f521e7e64b182e271a6f02edf1d1dabd20d
sarojiam17 <sarosebhandari100@gmail.com>
2025-07-02 04:04:24 +0545

    Updated both section

--- SectionA/bfs_dfs.py
+++ SectionA/bfs_dfs.py
@@ -59,10 +59,48 @@ def dfs(graph, start, goal):
             if child not in visited: 
                 stack.append(child)
 
+#BFS 
+def bfs(graph, start, goal):
+    visited =[]
+    queue = [start]
+    step = 0
 
-#driver code 
+    #print table header 
+    print("\n BFS Fringe Tracking in Table")
+    print("Step     | Current   | Fringe(Queue)               | Visited")
+    print("------------------------------------------------------------")
+
+    #BFS Traversal 
+    while queue: 
+        node = queue.pop(0) #take the front node 
+        step += 1
+        
+        if node not in visited: 
+            visited.append(node)
+
+            #diplay the current state 
+            print_table_row(step, node, queue, visited)
+
+        if node == goal: 
+            print(f"\n Goal found at {goal} with {step} steps \n\n ")
+            break 
+
+        #enqueue all childrens not already visited or queued 
+
+        for child in graph[node]:
+            if child not in visited and child not in queue: 
+                queue.append(child)
+
+
+
+
+#set the start point and goal node 
 start_node = 'A'
 goal_node = 'K'
 
+#Run DFS 
 dfs(graph, start_node, goal_node)
+
+#Run BFS 
+bfs(graph, start_node, goal_node)
 
